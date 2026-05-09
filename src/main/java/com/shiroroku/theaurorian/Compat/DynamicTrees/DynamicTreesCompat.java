bc1389f1c3ec06760a89ad62c7dffe351bbfacc4
Lonelyxiya <3438505487@qq.com>
2026-05-09 22:30:47 +0800

    Create DynamicTreesCompat.java

---
+++ src/main/java/com/shiroroku/theaurorian/Compat/DynamicTrees/DynamicTreesCompat.java
@@ -0,0 +1,16 @@
+package com.shiroroku.theaurorian.Compat.DynamicTrees;
+
+public class DynamicTreesCompat {
+    public static void preInit() {
+    }
+
+    public static void init() {
+    }
+
+    public static void clientPreInit() {
+    }
+
+    public static void clientInit() {
+
+    }
+}
